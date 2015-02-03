034fbd7bce443fba666dc4707e127795d299702d
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-02 21:33:15 -0800

    Calculate square of sums

---
+++ difference-of-squares/squares.rb
@@ -0,0 +1,13 @@
+class Squares
+  def initialize(integer)
+    @integer = integer
+  end
+
+  def square_of_sums
+    sum = 0
+    for i in 1..@integer
+      sum += i
+    end
+    sum**2
+  end
+end
