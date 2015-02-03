ce41ec0705b89fafd0bad046bf8da2aa0dd7201a
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-02 21:39:00 -0800

    All other tests passing
    :)

--- difference-of-squares/difference_of_squares_test.rb
+++ difference-of-squares/difference_of_squares_test.rb
@@ -15,32 +15,26 @@ class SquaresTest < MiniTest::Unit::TestCase
   end
 
   def test_square_of_sums_to_10
-    skip
     assert_equal 3025, Squares.new(10).square_of_sums
   end
 
   def test_sum_of_squares_to_10
-    skip
     assert_equal 385, Squares.new(10).sum_of_squares
   end
 
   def test_difference_of_sums_to_10
-    skip
     assert_equal 2640, Squares.new(10).difference
   end
 
   def test_square_of_sums_to_100
-    skip
     assert_equal 25_502_500, Squares.new(100).square_of_sums
   end
 
   def test_sum_of_squares_to_100
-    skip
     assert_equal 338_350, Squares.new(100).sum_of_squares
   end
 
   def test_difference_of_sums_to_100
-    skip
     assert_equal 25_164_150, Squares.new(100).difference
   end
 end
