e4af19cc50489713a163217eeddeed2dff49ac26
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-10 19:53:40 -0800

    Refactor gigasecond:
    Clean it up further using to_date

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -1,12 +1,9 @@
 require 'date'
 require 'time'
 
-#Checking class type is ugly. Is there a way to use duck typing?
 class Gigasecond
-  #Add number of days in a gigasecond
+  #Add a gigasecond
   def self.from(date_time)
-    date_time = date_time.to_time
-    date_time = date_time + (10**9)
-    Date.new(date_time.year, date_time.month, date_time.day)
+    (date_time.to_time + 10**9).to_date
   end
 end
