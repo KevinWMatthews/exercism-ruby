9443d3bbf910531728be80eb706cfbcbefbc6190
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-10 19:47:17 -0800

    Refactor gigasecond
    Use to_time to simplify logic greatly

--- gigasecond/gigasecond.rb
+++ gigasecond/gigasecond.rb
@@ -5,14 +5,8 @@ require 'time'
 class Gigasecond
   #Add number of days in a gigasecond
   def self.from(date_time)
-    if date_time.kind_of? Date
-      #Date class seems to default to adding minutes
-      #86400 seconds in a day
-      date_time = date_time + (10**9)/86400
-    elsif date_time.kind_of? Time
-      #Time class seems to default to adding seconds
-      date_time = date_time + (10**9)
-    end
+    date_time = date_time.to_time
+    date_time = date_time + (10**9)
     Date.new(date_time.year, date_time.month, date_time.day)
   end
 end
