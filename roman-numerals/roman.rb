8b252f05441861d58646294236e0b773dfd9d86d
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-10 20:12:25 -0800

    Roll back and try using gsub
    Utilizing a different approach to a solution

--- roman-numerals/roman.rb
+++ roman-numerals/roman.rb
@@ -1,29 +1,8 @@
 #Sweet! We can extend Ruby's built-in Integer class
 class Integer
   def to_roman
-    result = ""
-
-    tens = self / 10
-    result += "X" * (tens % 10)
-
-    ones = self % 10
-    if (ones == 1)
-     result += "I"*1
-    elsif (ones == 2)
-     result += "I"*2
-    elsif (ones == 3)
-     result += "I"*3
-    elsif (ones == 4)
-     result += "IV"
-    elsif (ones == 5)
-     result += "V"
-    elsif (ones == 6)
-     result += "VI"
-    elsif (ones == 7)
-      result += "VII"
-    elsif (ones == 9)
-     result += "IX"
-    end
+    result = "I" * (self % 5)
+    result.gsub!(/IIII/, "IV")
     result
   end
 end
