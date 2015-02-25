827781a31d7d72597fd1feff0886afc38b0001ac
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-24 18:37:37 -0800

    Generate completely random name
    Fails completely randomly, too

--- robot-name/robot.rb
+++ robot-name/robot.rb
@@ -2,12 +2,20 @@ class Robot
   attr_reader :name
 
   def initialize
-    @name = random_char + random_char + rand(1000).to_s
+    @name = random_name
+  end
+
+  def random_name
+    random_char + random_char + rand(1000).to_s
   end
 
   def random_char
     alphabet = Hash.new
     alphabet = ("A".."Z").map {|i| i}
     alphabet[rand(25)]
+  end
+
+  def reset
+    @name = random_name
   end
 end
