b507c90e77963ffb9f9d2b064a7c1fd154f40262
Kevin Matthews <KevinNotMatt@gmail.com>
2015-02-24 18:03:34 -0800

    Refactor difference-of-squares.rb to be rubyistic

--- difference-of-squares/squares.rb
+++ difference-of-squares/squares.rb
@@ -1,27 +1,17 @@
 class Squares
   def initialize(integer)
     @integer = integer
-    @square_sum = 0
-    @sum_square = 0
   end
 
   def square_of_sums
-    for i in 1..@integer
-      @square_sum += i
-    end
-    @square_sum = @square_sum**2
+    (1..@integer).inject(:+)**2
   end
 
   def sum_of_squares
-    for i in 1..@integer
-      @sum_square += i**2
-    end
-    @sum_square
+    (1..@integer).inject { |sum, int| sum + int**2 }
   end
 
   def difference
-    sum_of_squares
-    square_of_sums
-    @square_sum - @sum_square
+    square_of_sums - sum_of_squares
   end
 end
