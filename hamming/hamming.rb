09ac90307147e84c5ed5157068ef4136a3e6c007
Kevin Matthews <KevinNotMatt@gmail.com>
2015-03-10 19:44:23 -0700

    Refactor hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -4,17 +4,17 @@ class Hamming
       return -1
     end
 
-    errors = 0
+    distance = 0
     strand1.length.times do |i|
       if (!isValidBase(strand1[i]) || !isValidBase(strand2[i]))
         return -2
       end
 
       if (strand1[i] != strand2[i])
-        errors += 1
+        distance += 1
       end
     end
-    return errors
+    return distance
   end
 
   def self.isValidBase(base)
