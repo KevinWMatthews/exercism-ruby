d54fe243294f5e2e8fb8d3d88a306d9442e6b502
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-04-21 20:20:41 -0700

    Refactor nth-prime further
    Changes for readability:
       Use none? instead of any?
       Use divisible?

--- nth-prime/prime.rb
+++ nth-prime/prime.rb
@@ -17,20 +17,14 @@ class Prime
     dividend % divisor == 0
   end
 
-  def self.largest_factor_to_test(num)
-    (1+Math.sqrt(num))/6.0
-  end
-
-  def self.has_factor(num, k)
-    divisible?(num, 6*k+1) || divisible?(num, 6*k-1)
-  end
-
   def self.prime?(num)
     return num > 1 if num <= 3
     return false if (divisible?(num,2) || divisible?(num,3))
 
-    max_k = largest_factor_to_test(num)
-    return false if (1..max_k).any? {|k| has_factor(num,k)}
-    true
+    # Look up Primality Test on Wikipedia to see which factors we actually need to test:
+    #   Only need to test factors up to to the square root of num
+    #   Can step by 6 (yes, really!)
+    # If no factors are divisible, we have a prime number!
+    (5..Math.sqrt(num)).step(6).none? {|k| divisible?(num,k) || divisible?(num,k+2)}
   end
 end
