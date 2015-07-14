775b245195ae2a3212d16be0284cedab3aad9f76
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-07-13 17:38:52 -0700

    Revise hamming to use each_char

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -5,7 +5,7 @@ class Hamming
     end
 
     distance = 0
-    strand1.split("").zip(strand2.split("")) do |base1, base2|
+    strand1.each_char.zip(strand2.each_char) do |base1, base2|
       if (base1 != base2)
         distance += 1
       end
