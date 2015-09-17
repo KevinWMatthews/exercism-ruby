f0da802f4ddfec2f9ee7ac2bdefe27e706656eaa
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 19:20:53 -0700

    Refactor to use delete instead of select

--- word-count/tst.rb
+++ word-count/tst.rb
@@ -1,4 +1,7 @@
-s = "this^is^!@#word{}"
+s = "this^is^!@#9word{} '' ;as"
 c = s.each_char.select {|c| c.match(/[[:alpha:]]/)}
 puts c.inspect
 puts c.join('').inspect
+
+d = s.delete('^A-Za-z')
+puts d.inspect
--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -7,8 +7,7 @@ class Phrase
   def word_count
     word_hash = Hash.new(0)
     phrase.split.each do |word|
-      clean_chars = word.each_char.select {|c| c.match(/[[:alpha:]]/)}
-      clean_word = clean_chars.join
+      clean_word = word.delete('^A-Za-z') #Remove all non alphabetic characters
       next if clean_word.empty?
       word_hash[clean_word] = word_hash[clean_word] + 1
     end
