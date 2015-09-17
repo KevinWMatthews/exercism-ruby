a31e58ccbeed15ec8f6a57c628d19be957b8aa5a
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 21:32:13 -0700

    Respond to silence

--- bob/bob.rb
+++ bob/bob.rb
@@ -2,6 +2,7 @@ class Bob
   def hey(remark)
     return 'Whoa, chill out!' if remark.upcase == remark && remark.downcase != remark
     return 'Sure.' if remark[-1] == '?'
+    return 'Fine. Be that way!' if remark.strip.empty?
     'Whatever.'
   end
 end
