3a7e8faf9710242d027a3d15543143e7bd2829e8
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 18:39:07 -0700

    Restart word count

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -1,14 +1,9 @@
 class Phrase
-  attr_accessor :word_array, :word_hash
-  def initialize(string)
-    @string = string
-    word_array = []
+  attr_reader :phrase
+  def initialize(phrase)
+    @phrase = phrase
   end
 
   def word_count
-    word_array = @string.split(" ")
-
-    result = word_array.chunk { |w| word_array.count(w) }
-
   end
 end
