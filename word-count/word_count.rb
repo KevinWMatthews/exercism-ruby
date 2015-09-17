ec8c49db995619fecd6fcc8d56abdbdd88e2b889
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 21:17:44 -0700

    Refactor for clarity

--- word-count/word_count.rb
+++ word-count/word_count.rb
@@ -5,9 +5,10 @@ class Phrase
   end
 
   def word_count
+    words = phrase.downcase.split(/[ *,*]/)
     word_hash = Hash.new(0)
-    phrase.split(/[ *,*]/).each do |word|
-      clean_word = word.downcase.delete('^A-Za-z0-9\'')
+    words.each do |word|
+      clean_word = word.delete('^A-Za-z0-9\'')
       next if clean_word.empty?
       word_hash[clean_word] = word_hash[clean_word] + 1
     end
