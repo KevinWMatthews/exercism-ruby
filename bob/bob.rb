c9b962025cebac0754dad6b8cad9952dde4053be
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 21:30:57 -0700

    Obscure way to handle only numbers

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,6 +1,6 @@
 class Bob
   def hey(remark)
-    return 'Whoa, chill out!' if remark.upcase == remark
+    return 'Whoa, chill out!' if remark.upcase == remark && remark.downcase != remark
     return 'Sure.' if remark[-1] == '?'
     'Whatever.'
   end
