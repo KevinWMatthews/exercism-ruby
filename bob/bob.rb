e8fe7779b6cf2f7818db07a64502492e277e6d92
Kevin W Matthews <KevinWMatthews@gmail.com>
2015-09-16 21:37:39 -0700

    Refactor Bob

--- bob/bob.rb
+++ bob/bob.rb
@@ -1,6 +1,11 @@
 class Bob
+  def is_uppercase(string)
+    return false if string.downcase == string   #Occurs if string is all lowercase or all numeric
+    string.upcase == string
+  end
+
   def hey(remark)
-    return 'Whoa, chill out!' if remark.upcase == remark && remark.downcase != remark
+    return 'Whoa, chill out!' if is_uppercase(remark)
     return 'Sure.' if remark[-1] == '?'
     return 'Fine. Be that way!' if remark.strip.empty?
     'Whatever.'
